17b5a86934943bc5ce60be39a5481171fe7643b4
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2026-05-18 20:23:27 -0300

    feat(catalog): regras de dominio referente ao produto

---
+++ backend/CaminhoLivre/CaminhoLivre.Modulo.Catalago.Tests/Entites/ProdutoTest.cs
@@ -0,0 +1,35 @@
+﻿using CaminhoLivre.Modulo.Catalogo.Entities;
+
+namespace CaminhoLivre.Modulo.Catalago.Tests.Entites;
+
+[TestClass]
+public class ProdutoTest
+{
+
+    [TestMethod]
+    public void CriarProduto_Valido_DeveCriarComSucesso()
+    {
+        // Arrange
+        var nome = "Produto Teste";
+        var sku = "PRODUTO-TESTE";
+        var descricao = "Descrição do produto teste";
+        var precoCusto = 10.0m;
+        var precoVenda = 20.0m;
+
+        // Act
+        var produto = new Produto(nome, sku, descricao, precoCusto, precoVenda)
+        {
+            Ncm = "123",
+            Categoria = new Categoria("Categoria Teste"),
+            CategoriaId = 1,
+            Sku = sku,
+            Nome = nome,
+            Descricao = descricao,
+            PrecoVenda = precoVenda,
+            PrecoCusto = precoCusto
+        };
+
+        // Assert
+        Assert.IsNotNull(produto);
+    }
+}
--- backend/CaminhoLivre/CaminhoLivre.Modulo.Catalogo/Entities/Produto.cs
+++ backend/CaminhoLivre/CaminhoLivre.Modulo.Catalogo/Entities/Produto.cs
@@ -2,20 +2,51 @@
 
 public class Produto
 {
+    public Produto()
+    {
+        
+    }
+
+    public Produto(string nome, string sku, string descricao, decimal precoCusto, decimal precoVenda)
+    {
+        if(string.IsNullOrEmpty(nome))
+            throw new ArgumentException("O nome do produto é obrigatório.", nameof(nome));
+
+        if (string.IsNullOrEmpty(sku))
+            throw new ArgumentException("O SKU do produto é obrigatório.", nameof(sku));
+
+        if (precoCusto <= 0)
+            throw new ArgumentException("O preço de custo do produto deve ser maior que zero.", nameof(precoCusto));
+
+        if(precoVenda <= 0)
+            throw new ArgumentException("O preço de venda do produto deve ser maior que zero.", nameof(precoVenda));
+
+        if (string.IsNullOrEmpty(descricao))
+            throw new ArgumentException("Necessário a descrição do produto",nameof(descricao));
+
+        Ativo = true;
+        Sku = sku;
+        Nome = nome;
+        PrecoCusto = precoCusto;
+        PrecoVenda = precoVenda;
+        Descricao = descricao;
+        DataCadastro = DateTimeOffset.UtcNow;
+    }
+
     public long Id { get; set; }
     public required string Sku { get; set; }
     public required string Nome { get; set; }
     public required string Descricao { get; set; }
-    public string? CodigoBarras { get; set; }
+    public string CodigoBarras { get; set; }
     public required decimal PrecoVenda { get; set; }
     public required decimal PrecoCusto { get; set; }
     public decimal? PrecoPromocional { get; set; }
-    public string? Moeda { get; set; }
-    public decimal? PesoBruto { get; set; }
-    public required string Ncm { get; set; }
-    public required long Origem { get; set; }
+    public string Moeda { get; set; }
+    public decimal PesoBruto { get; set; }
+    public  string Ncm { get; set; }
+    public  long Origem { get; set; }
     public bool Ativo { get; set; }
-    public required DateTimeOffset DataCadastro { get; set; }
+    public  DateTimeOffset DataCadastro { get; set; }
     public DateTimeOffset? DataAlteracao { get; set; }
     public required long CategoriaId { get; set; }
     public required Categoria Categoria { get; set; }
