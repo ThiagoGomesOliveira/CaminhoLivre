33b145133ce95922eea24c73f99c95065e36d68f
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2026-05-18 19:43:06 -0300

    feat(catalogo): criação da dll de testes unitários

---
+++ backend/CaminhoLivre/CaminhoLivre.Modulo.Catalago.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace CaminhoLivre.Modulo.Catalago.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- backend/CaminhoLivre/CaminhoLivre.Modulo.Catalogo/Entities/Categoria.cs
+++ backend/CaminhoLivre/CaminhoLivre.Modulo.Catalogo/Entities/Categoria.cs
@@ -13,23 +13,17 @@ public class Categoria
             throw new ArgumentException("O nome da categoria é obrigatório.", nameof(nome));
 
         Nome = nome;
-        //Slug = GerarSlug(nome);
         Ativo = true;
         DataCadastro = DateTimeOffset.UtcNow;
     }
 
-    private string? GerarSlug(string nome)
-    {
-        throw new NotImplementedException();
-    }
-
     public long Id { get; set; }
-    public required string Nome { get; set; }
-    public string? Descricao { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
     public bool Ativo { get; set; }
-    public required DateTimeOffset DataCadastro { get; set; }
+    public  DateTimeOffset DataCadastro { get; set; }
     public DateTimeOffset? DataAlteracao { get; set; }
-    public required string  Slug { get; set; }
+    public string  Slug { get; set; }
 
     private readonly List<Produto> _produtos = new();
     public IReadOnlyCollection<Produto> Produtos => _produtos.AsReadOnly();
