8f16688d0a6a2837cead2e2bf99059928bb5c8e9
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2026-05-19 19:59:27 -0300

    feature(Inventario): Criação do módulo inventario

---
+++ backend/CaminhoLivre/CaminhoLivre.Modulo.Inventario/Entities/Estoque.cs
@@ -0,0 +1,12 @@
+﻿using CaminhoLivre.Modulo.Catalogo.Entities;
+
+namespace CaminhoLivre.Modulo.Inventario.Entities;
+
+public class Estoque
+{
+    public  long Id { get; set; }
+    public required long ProdutoId { get; set; }
+    public long QtdDisponivel { get; set; }
+    public long QtdReservada { get; set; }
+    public required Produto Produto { get; set; }
+}
